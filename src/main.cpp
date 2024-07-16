1261ce0aaccf11e5047db2431320ad077bcb2e0c
PauloMaced0 <paulomacedo@ua.pt>
2024-07-16 20:12:58 +0100

    refactor to c++

--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,36 @@
-#include <stdio.h>
+#include <iostream>
 
 #include "protocol.h"
 
-void print_msg (Message* message) {
-    if (message->type)
-        printf("\n%d\n", message->type);
-    else
-        printf("\ntype: (null)\n");
-
-    if (message->channel)
-        printf("%s\n", message->channel);
-    else
-        printf("channel: (null)\n");
-
-    if (message->user)
-        printf("%s\n", message->user);
-    else
-        printf("user: (null)\n");
-
-    if (message->message)
-        printf("%s\n", message->message);
-    else
-        printf("message: (null)\n");
-
-    if (message->timestamp)
-        printf("%s\n", asctime(localtime(&message->timestamp)));
-    else
-        printf("timestamp: (null)\n\n");
-}
-
 int main (void) {
-    Message* join_msg = create_join_message("test_channel");
-    Message* leave_msg = create_leave_message("test_channel");
-    Message* register_msg = create_register_message("test_user");
-    Message* text_msg = create_text_message("Hello, world!", "test_channel");
+    JoinMessage join_msg("test_channel");
+    LeaveMessage leave_msg("test_channel");
+    RegisterMessage register_msg("test_user");
+    TextMessage text_msg("Hello, world!", "test_channel");
 
-    uint16_t msg_len = 0;
-    int i = 0;
+    uniquePtr<JoinMessage> msg1 = JoinMessage::from_string(join_msg.to_string());
 
-    printf("Join message channel: ");
-    char* string = serialize_message(join_msg, &msg_len);
-    for (i = 0; i < msg_len; i++) {
-        printf("%#X ", string[i]);
-    }
-    Message* message = deserialize_message(string);
-    print_msg(message);
-    free(message);
-    free(string);
+    string str1 = join_msg.to_string();
+    string str2 = msg1->to_string();
+    if(str1.compare(str2) == 0)
+        std::cout << "Join message OK! " << str2 << std::endl;
 
-    msg_len = 0;
-    printf("Leave message channel: ");
-    string = serialize_message(leave_msg, &msg_len);
-    for (i = 0; i < msg_len; i++) {
-        printf("%#X ", string[i]);
-    }
-    message = deserialize_message(string);
-    print_msg(message);
-    free(string);
-    free(message);
+    uniquePtr<LeaveMessage> msg2 = LeaveMessage::from_string(leave_msg.to_string());
+    str1 = leave_msg.to_string();
+    str2 = msg2->to_string();
+    if(str1.compare(str2) == 0)
+        std::cout << "Leave message OK! " << str2 << std::endl;
 
-    msg_len = 0;
-    printf("Register message user: ");
-    string = serialize_message(register_msg, &msg_len);
-    for (i = 0; i < msg_len; i++) {
-        printf("%#X ", string[i]);
-    }
-    message = deserialize_message(string);
-    print_msg(message);
-    free(string);
-    free(message);
+    uniquePtr<RegisterMessage> msg3 = RegisterMessage::from_string(register_msg.to_string());
+    str1 = register_msg.to_string();
+    str2 = msg3->to_string();
+    if(str1.compare(str2) == 0)
+        std::cout << "Register message OK! " << str2 << std::endl;
 
-    msg_len = 0;
-    printf("Text message content: ");
-    string = serialize_message(text_msg, &msg_len);
-    for (i = 0; i < msg_len; i++) {
-        printf("%#X ", string[i]);
-    }
-    message = deserialize_message(string);
-    print_msg(message);
-    free(string);
-    free(message);
-
-    // Free the allocated memory
-    free_message(join_msg);
-    free_message(leave_msg);
-    free_message(register_msg);
-    free_message(text_msg);
-
+    uniquePtr<TextMessage> msg4 = TextMessage::from_string(text_msg.to_string());
+    str1 = text_msg.to_string();
+    str2 = msg4->to_string();
+    if(str1.compare(str2) == 0)
+        std::cout << "Text message OK! " << str2 << std::endl;
     return 0;
 }
