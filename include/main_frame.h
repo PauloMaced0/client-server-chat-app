fe2b90496d12ac91e5da5d50953aadf4ec73b69f
PauloMaced0 <paulomacedo@ua.pt>
2024-07-17 19:48:22 +0100

    app header files

---
+++ include/main_frame.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_FRAME_H_
+#define _MAIN_FRAME_H_
+
+#include "wx/string.h"
+#include <wx/wx.h>
+
+ 
+class MainFrame : public wxFrame
+{
+public:
+    MainFrame(const wxString &title);
+};
+
+#endif // !_MAIN_FRAME_H_
