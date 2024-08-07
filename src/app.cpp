c5674f5246d291f9cbd922a583da0c8c0b06e125
PauloMaced0 <paulomacedo@ua.pt>
2024-08-07 21:30:55 +0100

    rm print

--- src/app.cpp
+++ src/app.cpp
@@ -1,7 +1,5 @@
 #include "main_frame.h"
 #include "app.h"
-#include <iostream>
-#include <ostream>
 #include <wx/wx.h>
 
 wxIMPLEMENT_APP(App);
@@ -11,7 +9,6 @@ bool App::OnInit() {
     mainFrame->SetClientSize(900, 600);
     mainFrame->Center();
     mainFrame->Show();
-    std::cout << "Hello World from the terminal" << std::endl;
     return true;
 }
  
